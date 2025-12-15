a23a368448ebd7376f609163331353fe01c6ed14
appudura <achveiya.appuduraiedu.esiee.fr>
2025-12-15 22:16:19 +0000

    Display HTML page instead of text

--- index.js
+++ index.js
@@ -4,9 +4,34 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Render");
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My Render App</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background: #0f172a;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+          }
+          h1 {
+            color: #38bdf8;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Ì∫Ä App d√©ploy√©e avec succ√®s sur Render !</h1>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
